b5730f6d62b9d2216362ed3325bcf717fbf1d893
Masha Gorbunova <masha.gorbunova@gmail.com>
2023-03-16 19:00:45 +0300

    add -> add validation on commentText input

--- index.js
+++ index.js
@@ -28,8 +28,11 @@ function addComment() {
         actualDate = 'сегодня, ' + ' ' + actualTime
     } else if (userDate ===  todayDate-1 ) {
         actualDate = 'вчера, ' + ' ' + actualTime
+    } else if (!userDate) {
+        actualDate = timeConverterToday(Math.floor(Date.now()/1000)) + ', ' + actualTime;
     } else {
         actualDate = commentDate.value + ', ' + actualTime;
+
     }
 
     let comment = {
@@ -39,35 +42,13 @@ function addComment() {
         date: actualDate,
     }
 
-    // //check input Length
-    // function checkLength(input) {
-    //     if(input.value.length < 10) {
-    //         showError(input, `Текст комменатрия must be at least ${10} characters`);
-    //     }else if(input.value.length > 200) {
-    //         showError(input, `Текст комменатрия must be les than ${200} characters`);
-    //     }else {
-    //         showSucces(input);
-    //     }
-    // }
-
-// //show success colour
-//     function showSucces(input) {
-//         const formControl = input.parentElement;
-//         formControl.className = 'form-control success';
-//     }
-// //Show input error messages
-//     function showError(input, message) {
-//         const formControl = input.parentElement;
-//         formControl.className = 'form-control error';
-//         const small = formControl.querySelector('small');
-//         small.innerText = message;
-//     }
-//
-//     checkLength(commentText.value);
-
-    comments.push(comment);
-    saveCommentsToLS();
-    setCommentsFromLS();
+    if(commentText.value.length < 10) {
+        checkLength(commentText);
+    } else {
+        comments.push(comment);
+        saveCommentsToLS();
+        setCommentsFromLS();
+    }
 }
 
 
@@ -86,17 +67,17 @@ function setCommentsFromLS(){
     let commentField = document.getElementById('comment-field');
     let out = '';
     comments.map( item => {
-        out += `<div class="test">
+        out += `<div class="result-field">
                         <p>Дата: ${item.date}</p>
                         <p>Имя: ${item.name}</p>
                         <p>Комментарий: ${item.text}</p>
-                        <button id="comment-delete" onClick="removeComment(${item.id})">🗑</button>
-                        <button id="comment-like">💖</button>
+                        <button class="comment-bin" id="comment-delete" onClick="removeComment(${item.id})">
+                        <img class="bin-png" src="asserts/bin.png" alt="bin svg"/>
+                        </button>
+                        <button class="comment-like" id="comment-like" onClick="like()">
+                        <img class="like-png" src="asserts/like.png" alt="like svg"/>
+                        </button>
                 </div>`
-        // out += `<p>Имя: ${item.name}</p>`
-        // out += `<p>Комментарий: ${item.text}</p>`
-        // out += `<button id="comment-delete" onClick="removeComment(${item.id})">🗑</button>`
-        // out += `<button id="comment-like">💖</button>`
     });
 
     commentField.innerHTML = out;
@@ -112,10 +93,16 @@ function removeComment(id) {
 
 function timeConverter(UNIX_timestamp){
     let a = new Date(UNIX_timestamp * 1000);
-    // let months = ['01', '02' , '03','04','05','06','07','08','09','10','11','12'];
-    // let month = months[a.getMonth()];
-    // let date = a.getDate();
     return a.getDate();
+}
+
+function timeConverterToday(UNIX_timestamp){
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['01', '02' , '03','04','05','06','07','08','09','10','11','12'];
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let year = a.getFullYear();
+    return date + '-' + month + '-' + year;
 }
 
 function getHoursAndMinutes(UNIX_timestamp){
@@ -124,6 +111,26 @@ function getHoursAndMinutes(UNIX_timestamp){
     let min = a.getMinutes();
     return hour + ':' + min;
 }
+
+function like() {
+    alert('спасибо за лайк!')
+}
+
+//Show input error messages
+    function showError(input, message) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control error';
+        const small = formControl.querySelector('small');
+        small.innerText = message;
+    }
+
+//check input Length
+function checkLength(input) {
+    showError(input, `Введите минимум ${10} символов`);
+}
+
+
+
 
 
 
