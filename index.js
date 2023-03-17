4e2c065da289ab3fbf206d3f18b0f7dd179f3ef7
Masha Gorbunova <masha.gorbunova@gmail.com>
2023-03-17 14:46:11 +0300

    add -> cancel error after posting comment

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ function addComment() {
     let commentDate = document.getElementById('comment-date');
 
 
-    let todayDate = timeConverter(Math.floor(Date.now()/1000));
+    let todayDate = timeConverter(Math.floor(Date.now() / 1000));
     let userDate = Number(commentDate.value.split('').reverse().splice(0, 2).reverse().join(''));
 
     let actualDate = '';
-    let actualTime = getHoursAndMinutes(Math.floor(Date.now()/1000))
-    if(userDate === todayDate) {
+    let actualTime = getHoursAndMinutes(Math.floor(Date.now() / 1000))
+    if (userDate === todayDate) {
         actualDate = 'сегодня, ' + ' ' + actualTime
-    } else if (userDate ===  todayDate-1 ) {
+    } else if (userDate === todayDate - 1) {
         actualDate = 'вчера, ' + ' ' + actualTime
     } else if (!userDate) {
-        actualDate = timeConverterToday(Math.floor(Date.now()/1000)) + ', ' + actualTime;
+        actualDate = timeConverterToday(Math.floor(Date.now() / 1000)) + ', ' + actualTime;
     } else {
         actualDate = commentDate.value + ', ' + actualTime;
 
@@ -42,7 +42,8 @@ function addComment() {
         date: actualDate,
     }
 
-    if(commentText.value.length < 10) {
+    cancelError(commentText)
+    if (commentText.value.length < 10) {
         checkLength(commentText);
     } else {
         comments.push(comment);
@@ -52,21 +53,21 @@ function addComment() {
 }
 
 
-function saveCommentsToLS(){
+function saveCommentsToLS() {
     localStorage.setItem('comments', JSON.stringify(comments));
 }
 
-function loadComments(){
+function loadComments() {
     if (localStorage.getItem('comments'))
         comments = JSON.parse(localStorage.getItem('comments'));
 
     setCommentsFromLS();
 }
 
-function setCommentsFromLS(){
+function setCommentsFromLS() {
     let commentField = document.getElementById('comment-field');
     let out = '';
-    comments.map( item => {
+    comments.map(item => {
         out += `<div class="result-field">
                         <p>Дата: ${item.date}</p>
                         <p>Имя: ${item.name}</p>
@@ -90,22 +91,21 @@ function removeComment(id) {
 }
 
 
-
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp * 1000);
     return a.getDate();
 }
 
-function timeConverterToday(UNIX_timestamp){
+function timeConverterToday(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp * 1000);
-    let months = ['01', '02' , '03','04','05','06','07','08','09','10','11','12'];
+    let months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     let month = months[a.getMonth()];
     let date = a.getDate();
     let year = a.getFullYear();
     return date + '-' + month + '-' + year;
 }
 
-function getHoursAndMinutes(UNIX_timestamp){
+function getHoursAndMinutes(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp * 1000);
     let hour = a.getHours();
     let min = a.getMinutes();
@@ -116,17 +116,23 @@ function like() {
     alert('спасибо за лайк!')
 }
 
-//Show input error messages
-    function showError(input, message) {
-        const formControl = input.parentElement;
-        formControl.className = 'form-control error';
-        const small = formControl.querySelector('small');
-        small.innerText = message;
-    }
 
-//check input Length
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
 function checkLength(input) {
-    showError(input, `Введите минимум ${10} символов`);
+    showError(input, `Введите минимум 10 символов`);
+}
+
+function cancelError(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control small';
+    const small = formControl.querySelector('small');
+    small.innerText = '';
 }
 
 
